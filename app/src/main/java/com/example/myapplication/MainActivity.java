d4380fa1c4390732f5ff99cbd344ad84a8ec00c0
radenjaelaniakbar <jaelaniakbar99@gmail.com>
2021-10-24 07:57:17 -0700

    khartasis apps 5

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btnNext;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        btnNext = (Button) findViewById(R.id.btnNext);
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent ( MainActivity.this, genderActivity2.class);
+                startActivity(i);
+
+            }
+        });
+
+
+    }
+}
