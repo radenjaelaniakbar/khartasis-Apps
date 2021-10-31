6a07687d13db268799c886287fb5de664763bb9c
Gombeng <msrgombeng@gmail.com>
2021-10-31 17:44:51 +0700

    adding page hasil test

---
+++ app/src/main/java/com/example/myapplication/music_feel_good.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class music_feel_good extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_music_feel_good);
+    }
+}
