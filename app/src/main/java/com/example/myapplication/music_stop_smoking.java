af33e8edf094e6d6c8ba6890dcf810064acdd1f9
Gombeng <msrgombeng@gmail.com>
2021-10-31 16:58:42 +0700

    adding page stop smoking

---
+++ app/src/main/java/com/example/myapplication/music_stop_smoking.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class music_stop_smoking extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_music_stop_smoking);
+    }
+}
